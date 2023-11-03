72c3f9982b27ae6f38d46f329066fec3729b0f32
Nonthapat Kaewamporn <tontan2545@gmail.com>
2023-11-03 12:48:36 +0700

    feat: add geist font

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,12 +1,14 @@
 import "@ui/styles/globals.css";
 
+import { GeistSans, GeistMono } from "geist/font";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body>{children}</body>
     </html>
   );
