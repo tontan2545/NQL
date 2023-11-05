482e75fcfedd5cc8a39dd2b803d4c423a9d6f8a0
Nonthapat Kaewamporn <tontan2545@gmail.com>
2023-11-05 11:41:50 +0700

    feat: added prisma

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { InferenceModule } from './inference/inference.module';
+import { LLMService } from './llm/llm.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    InferenceModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, LLMService],
 })
 export class AppModule {}
