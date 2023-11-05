8940aa29495b6e787d1d04b7d4a5935c26a721dc
tontan2545 <tontan2545@gmail.com>
2023-11-06 01:20:10 +0700

    feat: added openapi docs

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('NQL API')
+    .setDescription('Set of REST APIs for NQL')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8000);
 }
 bootstrap();
