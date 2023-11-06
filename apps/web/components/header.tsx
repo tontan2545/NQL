aaf7d1168e5a81028e851ec62bdc5be41e47e411
tontan2545 <tontan2545@gmail.com>
2023-11-07 02:08:37 +0700

    feat: header with gradient

--- apps/web/components/header.tsx
+++ apps/web/components/header.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@ui/components/ui/button";
+import { Button } from "@ui/components/button";
 import { ArrowRight, Bot } from "lucide-react";
 import Link from "next/link";
 import React from "react";
@@ -6,7 +6,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="flex flex-col items-center w-full">
-      <div className="flex text-base bg-gradient-to-r to-orange-400 via-rose-600 from-rose-700 text-white w-full justify-center py-1">
+      <div className="flex text-base bg-gradient-to-r to-orange-400 via-rose-500 from-rose-700 text-white w-full justify-center py-1">
         <Link
           href="https://web.facebook.com/ISEopenhouse/posts/pfbid02LydzRUfsJdg7w2YtNeysMMK33rvyZ597WwG4EFYmvVJMk9Yj55Q4rMTxTZpyNRHjl"
           target="_blank"
@@ -34,7 +34,9 @@ const Header = () => {
         <div className="flex text-xl space-x-1">
           <h4>Supercharge data analytics with the power of</h4>
           <div className="relative">
-            <h4 className="z-10 font-semibold italic">Generative AI</h4>
+            <h4 className="z-10 font-bold bg-gradient-to-r to-orange-400 via-rose-400 from-rose-600 text-transparent bg-clip-text animate-gradient">
+              Generative AI
+            </h4>
             {/* <div className="absolute bg-primary/40 w-[106%] h-[5px] bottom-[3px] z-0 left-[-3%]" /> */}
           </div>
         </div>
