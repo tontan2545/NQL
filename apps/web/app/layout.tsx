5fe0f5dde43278edf08befffe8f781ad5d214316
tontan2545 <tontan2545@gmail.com>
2023-11-06 21:53:57 +0700

    feat: children to take up remaining height

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,19 @@
-import "@ui/styles/globals.css";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import "@nql/ui/styles/globals.css";
+import type { Metadata } from "next";
 
 import { GeistSans, GeistMono } from "geist/font";
+
+export const metadata: Metadata = {
+  title: "NQL",
+  description: "NQL Text-to-SQL solution using Amazon Bedrock",
+  authors: [
+    {
+      name: "Nonthapat Kaewamporn",
+    },
+  ],
+};
 
 export default function RootLayout({
   children,
@@ -9,7 +22,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
-      <body>{children}</body>
+      <body className="flex flex-col min-h-screen">
+        <Header />
+        <div className="flex flex-col flex-1">{children}</div>
+        <Footer />
+      </body>
     </html>
   );
 }
