4441c5dc6fcd3d1635ee4d3902312a7004659b7e
tontan2545 <tontan2545@gmail.com>
2023-11-06 21:54:50 +0700

    feat: component integration

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,25 @@
-import { Button } from "@ui/components/button";
+import Search from "@/components/search";
+import SQL from "@/components/sql";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@ui/components/tabs";
 
 export default function Page() {
   return (
-    <>
-      <h1>Web</h1>
-      <Button>Click me</Button>
-    </>
+    <div className="flex flex-col w-full items-center pt-7">
+      <div className="w-2/5 space-y-4">
+        <Search />
+        <Tabs defaultValue="SQL" className="w-full">
+          <TabsList className="shadow-sm">
+            <TabsTrigger value="SQL">SQL</TabsTrigger>
+            <TabsTrigger value="Data">Data</TabsTrigger>
+          </TabsList>
+          <TabsContent
+            value="SQL"
+            className="shadow-sm bg-transparent border-[1px] border-border rounded-md p-8"
+          >
+            <SQL />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
   );
 }
