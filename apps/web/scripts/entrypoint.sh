97b0ed32c5f31c2991e7b11f6d2657e3b67d5446
tontan2545 <tontan2545@gmail.com>
2023-11-08 02:00:12 +0700

    feat: working env var replacement in web

---
+++ apps/web/scripts/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# no verbose
+set +x
+# config
+envFilename='./apps/web/.env.production'
+nextFolder='./apps/web/.next/'
+
+echo "Apply path"
+
+while read -r line; do
+  # no comment or not empty
+  echo "Check: $line"
+  if [ "${line:0:1}" == "#" ] || [ "${line}" == "" ]; then
+    continue
+  fi
+
+  # split
+  configName="$(cut -d'=' -f1 <<<"$line")"
+  configValue="$(cut -d'=' -f2 <<<"$line")"
+  # get system env
+  envValue=$(env | grep "^$configName=" | grep -oe '[^=]*$')
+
+  echo "Config: ${configName} = ${configValue}"
+
+  # if config found
+  if [ -n "$configValue" ] && [ -n "$envValue" ]; then
+    # replace all
+    echo "Replace: ${configValue} with: ${envValue}"
+    find $nextFolder \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#$configValue#$envValue#g"
+  fi
+done <$envFilename
+
+echo "Starting Nextjs"
+exec "$@"
