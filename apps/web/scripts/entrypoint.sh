70034dc92c5267dc5eb153079eb1bc8ee552b58b
tontan2545 <tontan2545@gmail.com>
2023-11-08 19:51:38 +0700

    feat: remove loggin

--- apps/web/scripts/entrypoint.sh
+++ apps/web/scripts/entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-# no verbose
-set +x
 # config
 envFilename='./apps/web/.env.production'
 nextFolder='./apps/web/.next/'
 
-echo "Apply path"
-
 while read -r line; do
   # no comment or not empty
-  echo "Check: $line"
   if [ "${line:0:1}" == "#" ] || [ "${line}" == "" ]; then
     continue
   fi
@@ -20,12 +15,9 @@ while read -r line; do
   # get system env
   envValue=$(env | grep "^$configName=" | grep -oe '[^=]*$')
 
-  echo "Config: ${configName} = ${configValue}"
-
-  # if config found
+  # if config found && configName starts with NEXT_PUBLIC
   if [ -n "$configValue" ] && [ -n "$envValue" ]; then
     # replace all
-    echo "Replace: ${configValue} with: ${envValue}"
     find $nextFolder \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#$configValue#$envValue#g"
   fi
 done <$envFilename
